cea38c6bd495fa9abc9564afe97734e441303c7b
Achraf <achraf.rehioui.1@gmail.com>
2021-02-13 16:49:10 +0100

    setup materials-ui

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import React, { useState } from 'react';
-import './App.css';
-import Breadcrumb from './components/Breadcrumb';
+import React from "react";
+import "./styles.css";
+import Home from "./Home";
+import About from "./About";
+import { Route, Switch } from "react-router-dom";
+import Jobs from "./Jobs";
+import Engineer from "./Engineer";
+import Marketer from "./Marketer";
+import Designer from "./Designer";
+import Breadcrumbs from "./Breadcrumbs";
 
-function App() {
-
-  const [crumbs, setCrumbs] = useState(['Home', 'Category', 'Sub Category']);
-
-  const selected = crumb => {
-    console.log(crumb);
-  }
-
+export default function App() {
   return (
-    <div className="App container">
-      <Breadcrumb crumbs={crumbs} selected={selected} />
-    </div>
+    <>
+      <Breadcrumbs />
+      <Switch>
+        <Route exact from="/" render={props => <Home {...props} />} />
+        <Route exact path="/about" render={props => <About {...props} />} />
+        <Route exact path="/jobs" render={props => <Jobs {...props} />} />
+        <Route
+          exact
+          path="/jobs/engineer"
+          render={props => <Engineer {...props} />}
+        />
+        <Route
+          exact
+          path="/jobs/marketer"
+          render={props => <Marketer {...props} />}
+        />
+        <Route
+          exact
+          path="/jobs/designer"
+          render={props => <Designer {...props} />}
+        />
+      </Switch>
+    </>
   );
 }
-
-export default App;
