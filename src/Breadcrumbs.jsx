da42416f0435b5f74bd78ec32f92b5d594621dab
Achraf <achraf.rehioui.1@gmail.com>
2021-02-16 13:13:26 +0100

    implement MUI Breadcrumb with React Router

--- src/Breadcrumbs.jsx
+++ src/Breadcrumbs.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import {Breadcrumbs as MUIBreadcrumbs}from '@material-ui/core';
-import Link from '@material-ui/core/Link';
+import { Breadcrumbs as MUIBreadcrumbs, Link, Typography }from '@material-ui/core';
+import { withRouter } from 'react-router-dom';
 
-function handleClick(event) {
-  event.preventDefault();
-  console.info('You clicked a breadcrumb.');
-}
+const Breadcrumbs = props => {
 
-export default function SimpleBreadcrumbs() {
+  const { 
+    history, 
+    location : { pathname } 
+  } = props;
+
+  const pathnames = pathname.split('/').filter(x => x);
+
   return (
     <MUIBreadcrumbs aria-label="breadcrumb">
-      <Link color="inherit" href="/" onClick={handleClick}>
-        Material-UI
-      </Link>
-      <Link 
-        color="inherit" 
-        href="/getting-started/installation/" 
-        onClick={handleClick}
-        >
-
-        Core
-      </Link>
-      <Typography color="textPrimary">Breadcrumb</Typography>
+      { pathnames.length > 0 ? (
+      <Link onClick={() => history.push("/") }>Home</Link>
+      ) : ( <Typography> Home </Typography> 
+      )}
+      {pathnames.map((name, index) => {
+          const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
+          const isLast = index === pathnames.length - 1;
+          return isLast ? (
+          <Typography key={name}> {name} </Typography>
+          ) : ( <Link key ={name} onClick={() => history.push(routeTo)}>{name}</Link>);
+      })}
     </MUIBreadcrumbs>
   );
 
 }
+
+export default withRouter(Breadcrumbs);
